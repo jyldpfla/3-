c8e8d8b0e63110595324801ee3dca4439724f838
ino456852 <ino456852@gmail.com>
2025-06-19 23:23:30 +0900

    2025.06.19(목) 백인호 1. 팀원추가 기능 구현 2. 팀원 목록 조회기능 구현

---
+++ inho.py
@@ -0,0 +1,68 @@
+from flask import *
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# env 파일 로드
+load_dotenv()
+user = os.getenv("USER")
+uri = f"mongodb+srv://{user}@team3.fxbwcnh.mongodb.net/"
+
+app = Flask(__name__)
+
+client = MongoClient(uri)
+db = client['team3']
+
+project_collection = db["projects"]
+user_collection = db["users"]
+schedule_collection = db["schedules"]
+personal_todo_collection = db["personal_todo"]
+board_collection = db["board"]
+team_collection = db["team"]
+
+@app.route("/")
+def home():
+    return render_template("/index.html")
+
+@app.route("/example")
+def example():
+    return render_template("/example.html")
+
+
+
+@app.route('/teamMemberAdd', methods=["GET", "POST"])
+def teamMemberAdd():
+    if request.method == "POST":
+        name = request.form.get("name")
+        status = request.form.get("status")
+
+        user_info = user_collection.find_one({"name": name})
+        if user_info:
+            team_collection.insert_one({
+                "name": name,
+                "email": user_info.get("email"),
+                "role": user_info.get("role"),
+                "department": user_info.get("department"),
+                "status": status
+            })
+            return redirect(url_for("teamMemberManage"))
+        else:
+            return "해당 이름의 유저를 찾을 수 없습니다.", 404
+
+    users = list(user_collection.find({}, {"_id": 0})) 
+    return render_template("teamMembersAdd.html", users=users)
+
+
+@app.route('/teamMemberManage')
+def teamMemberManage():
+    team_members = list(team_collection.find({}, {'_id': 0}))  
+    return render_template('teamMembersManage.html', team_members=team_members)
+
+
+@app.route('/project-detail') # 원지님 코드
+def project_detail():
+    return render_template("/projectDetail.html")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
