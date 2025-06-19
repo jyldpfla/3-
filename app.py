b800f3bce9d42d3c787d977a35f5412d98f920c5
ino456852 <ino456852@gmail.com>
2025-06-19 17:17:27 +0900

    2025.06.19(목) 백인호 1. teamMemberAdd ,teamMemberManage  html, css추가 2 projectDetail html css 추가 3. app.py에 함수 생성

--- app.py
+++ app.py
@@ -28,5 +28,39 @@ def home():
 def example():
     return render_template("/example.html")
 
+
+
+@app.route('/team-member-add', methods=["GET", "POST"])
+def team_member_add():
+    # if request.method == "POST":
+        # name = request.form.get("name")
+        # status = request.form.get("status")
+
+        # users 컬렉션에서 이름에 해당하는 사용자 정보 찾기
+        # user_info = user_collection.find_one({"name": name})
+        # if user_info:
+        #     team_collection.insert_one({
+        #         "name": name,
+        #         "email": user_info.get("email"),
+        #         "role": user_info.get("role"),
+        #         "department": user_info.get("department"),
+        #         "status": status
+        #     })
+        #     return redirect(url_for("home")) 
+        # else:
+        #     return "해당 이름의 유저를 찾을 수 없습니다.", 404
+    return render_template('/teamMembersAdd.html')
+
+@app.route('/team-member-manage')
+def team_member_manage():
+    
+    return render_template('/teamMembersManage.html')
+
+
+@app.route('/project-detail')
+def project_detail():
+    return render_template("/projectDetail.html")
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
