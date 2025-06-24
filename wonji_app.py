d0fa9049e81ff415f4e675f70626441ee32a1427
DESKTOP-SQMFRJ3\pc <sjkgrace82@gmail.com>
2025-06-24 10:09:00 +0900

    2025. 06. 24 (화) 박원지 1. 프로젝트 삭제, 수정, 추가할 수 있는 코드

--- wonji_app.py
+++ wonji_app.py
@@ -29,28 +29,56 @@ def home():
 def example():
     return render_template("/example.html")
 
-@app.route("/project")
-def project():
+@app.route("/projectList")
+def projectList():
     project_list = list(project_collection.find())
     done = [t for t in project_list if t['status'] == '완료']
     doing = [t for t in project_list if t['status'] == '진행중']
     wait = [t for t in project_list if t['status'] == '진행 대기']
     for project in project_list:
         project["project_manager"] = user_collection.find_one({"_id": project["project_manager"]})["name"]
-    return render_template("/Allproject.html", project_list=project_list, done=done, doing=doing, wait=wait)
+    return render_template("/projectList.html", project_list=project_list, done=done, doing=doing, wait=wait)
 
-@app.route("/project2")
-def project2():
+@app.route("/projectAdd", methods=["GET", "POST"])
+def projectAdd():
+    if request.method == "POST":
+        project = {
+            "title": request.form.get("name"), 
+            "client": request.form.get("client"),  
+            "project_manager": ObjectId(request.form.get("project_manager")), 
+            "schedule.start_date": request.form.get("start"), 
+            "schedule.end_date": request.form.get("end"), 
+            "status": request.form.get("status"),  
+            "description": request.form.get("description")  
+        }
+        project_collection.insert_one(project)  
+        return redirect(url_for('projectList')) 
+
     user_list = list(user_collection.find({"position": "팀장"}))
-    print(user_list)
-    return render_template("/Allproject2.html", user_list=user_list)
+    return render_template("/projectAdd.html", user_list=user_list)
 
-@app.route("/page/<project_id>")
-def page(project_id):
+
+@app.route("/projectUpdate/<project_id>", methods=["GET", "POST"])
+def projectUpdate(project_id):
+    if request.method == "POST":
+        project_collection.update_one(
+            {"_id": ObjectId(project_id)},
+            {"$set": {
+                "title": request.form.get("name"),
+                "client": request.form.get("client"),
+                "schedule.start_date": request.form.get("start"),
+                "schedule.end_date": request.form.get("end"),
+                "status": request.form.get("status"),
+                "description": request.form.get("description")
+            }}
+        )
+        return redirect(url_for('projectDetail', project_id=project_id))
     project = project_collection.find_one({"_id": ObjectId(project_id)})
     manager = user_collection.find_one({"_id": project["project_manager"]})["name"]
-    project["manager_name"] =  manager
-    return render_template("/page.html", project=project)
+    project["manager_name"] = manager
+
+    return render_template("/projectUpdate.html", project=project)
+
 
 @app.route('/projectDetail/<project_id>') # 원지님 코드
 def projectDetail(project_id):
@@ -58,48 +86,13 @@ def projectDetail(project_id):
     print(project)
     manager = user_collection.find_one({"_id": project["project_manager"]})["name"]
     project["manager_name"] =  manager
-
     return render_template("/projectDetail.html", project=project)
 
-
-@app.route("/project/add", methods=["POST"])
-def add_project():
-    project = {
-        "title": request.form.get("name"),
-        "client": request.form.get("client"),
-        "project_manager": ObjectId(request.form.get("project_manager")),
-        "schedule.start_date": request.form.get("start"),
-        "schedule.end_date": request.form.get("end"),
-        "status": request.form.get("status"),
-        "description": request.form.get("description")
-    }
-    project_collection.insert_one(project)
-    return redirect(url_for('project'))
-    
-
-
-@app.route('/project/update/<project_id>', methods=['POST'])
-def update_project(project_id):
-    project_collection.update_one(
-        {"_id": ObjectId(project_id)},
-        {"$set": {
-            "title": request.form.get("name"),
-            "client": request.form.get("client"),
-            "members": request.form.get("members"),
-            "schedule.start_date": request.form.get("start"),
-            "schedule.end_date": request.form.get("end"),
-            "status": request.form.get("status"),
-            "description": request.form.get("description")
-        }}
-    )
-    return redirect(url_for('projectDetail.html', project_id=project_id))
-
-
-
-
+@app.route("/projectDelete/<project_id>", methods=["POST"])
+def projectDelete(project_id):
+    project_collection.delete_one({"_id": ObjectId(project_id)})
+    return redirect(url_for('projectList'))
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
-    
-    
